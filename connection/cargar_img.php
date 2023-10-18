8b6209caffeb4961ad8db2013f62a9202485dc53
TheIdolJavox <javierespana877@gmail.com>
2023-10-18 01:46:53 -0600

    Avance7
    Se permite subir a la base de datos la información de cada prenda, también ya esta la interfaz para la subida de archivos

--- connection/cargar_img.php
+++ connection/cargar_img.php
@@ -1,34 +1,41 @@
 <?php
-   $host        = "localhost";
-   $port        = "5432";
-   $dbname      = "DressRent";
-   $user        = "postgres";
-   $password    = "Estillo1216.";
+$host        = "localhost";
+$port        = "5432";
+$dbname      = "DressRent";
+$user        = "postgres";
+$password    = "Estillo1216.";
 
-   // Conectar a la base de datos PostgreSQL
-   $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+// Conectar a la base de datos PostgreSQL
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
-   if (!$conn) {
-      die("Error en la conexión a la base de datos: " . pg_last_error());
+if (!$conn) {
+    die("Error en la conexión a la base de datos: " . pg_last_error());
 }
 
-   $descripcion = $_POST['descripcion'];
-   $tipo = $_POST['tipo'];
-   $existencias = $_POST['existencias'];
+if (
+    isset($_POST['id_prenda']) && isset($_POST['descripcion']) && isset($_POST['tipo']) && isset($_POST['existencias']) && isset($_FILES['imagen'])
+) {
+    $id_prenda = $_POST['id_prenda'];
+    $descripcion = $_POST['descripcion'];
+    $tipo = $_POST['tipo'];
+    $existencias = $_POST['existencias'];
 
-   // Procesar la imagen
-   $imagen_data = file_get_contents($_FILES['imagen']['tmp_name']);
+    // Procesar la imagen
+    $imagen_data = bin2hex(file_get_contents($_FILES['imagen']['tmp_name']));
 
-   // Insertar la imagen en la base de datos
-   $query = "INSERT INTO prendas (descripcion, tipo, existencias, imagen) VALUES ($1, $2, $3, $4, $5, $6)";
-   $result = pg_query_params($conn, $query, [$descripcion, $tipo, $existencias, $imagen_data]);
+    // Insertar la imagen en la base de datos
+    $query = "INSERT INTO prendas (id_prenda, descripcion, tipo, existencias, imagen) VALUES ($1, $2, $3, $4, decode($5, 'hex'))";
+    $result = pg_query_params($conn, $query, [$id_prenda, $descripcion, $tipo, $existencias, $imagen_data]);
 
-   if ($result) {
-       echo "Imagen insertada con éxito.";
-   } else {
-       echo "Error al insertar la imagen: " . pg_last_error();
-   }
+    if ($result) {
+        echo "Imagen insertada con éxito.";
+    } else {
+        echo "Error al insertar la imagen: " . pg_last_error();
+    }
 
-   // Cerrar la conexión a la base de datos
-   pg_close($conn);
+    // Cerrar la conexión a la base de datos
+    pg_close($conn);
+} else {
+    echo "Faltan campos en el formulario.";
+}
 ?>
